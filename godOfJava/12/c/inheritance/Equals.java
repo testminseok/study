d40d8c5c74ac18cdf991a8a727547f2d8214af58
testMinseok <opso_@naver.com>
2022-08-12 18:39:55 +0900

    자바의신 12장 ObjectClass 의 hashCode()

--- godOfJava/12/c/inheritance/Equals.java
+++ godOfJava/12/c/inheritance/Equals.java
@@ -13,6 +13,14 @@ public class Equals {
 			
 			equals객체를 Overriding 했다면, hashCode()도 Overriding 해야한다.
 			equals의 결과로 객체가 서로 같다고는 할 수 있지만, 그 값이 같다고해서 주소값이 같지는 않기 때문이다.
+			
+			hashCode()
+			1. 자바 어플리케이션이 수행되는 동안에 어떤 객ㅊ에에 대해서 이 메소드가 호출될때에는 항상 동일한 int 값을 리턴해 주어야 한다. 
+				하지만, 자바를 실행할 때마다 같은 값이어야 할 필요는 없다.
+			2. 어떤 두개의 객체에 대해서 equals() 메소드를 사용하여 비교한 결과가 true 일 경우에 , 
+				두 객체의 hashCode() 메소드를 호출하면 동일한 int값을 리턴해야만한다.
+			3. 두 객체를 equals() 메소드를 사용하여 비교한 결과 false를 리턴했다고 해서, hashCode() 메소드를 호출한 int값이 무조건 달라야 할 필요는 없다. 
+				하지만, 이 경우에 서로 다른 int값을 제공하면 hashTable의 성능을 향상시키는데 도움이 된다.
 		*/
 		Equals thisObject = new Equals();
 		thisObject.equalsMethod();
