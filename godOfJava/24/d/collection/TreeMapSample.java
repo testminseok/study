8fb1124114685bb0aad2dced03c710c22fc5fc17
testMinseok <opso_@naver.com>
2022-08-23 17:19:41 +0900

    Collection Map TreeMap

---
+++ godOfJava/24/d/collection/TreeMapSample.java
@@ -0,0 +1,28 @@
+package d.collection;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class TreeMapSample {
+    public static void main(String[] args) {
+        TreeMapSample sample = new TreeMapSample();
+        sample.checkTreeMap();
+    }
+
+    public void checkTreeMap() {
+        /*
+            키가 String 일때 정렬되는 기본적인순서는 숫자 > 알파벳 대문자 > 알파벳 소문자 > 한글
+         */
+        TreeMap<String, String> map = new TreeMap<>();
+        map.put("A", "a");
+        map.put("가", "e");
+        map.put("1", "f");
+        map.put("a", "g");
+
+        Set<Map.Entry<String, String>> entries = map.entrySet();
+        for (Map.Entry<String, String> entry : entries) {
+            System.out.println(entry.getKey() + " = " + entry.getValue());
+        }
+    }
+}
