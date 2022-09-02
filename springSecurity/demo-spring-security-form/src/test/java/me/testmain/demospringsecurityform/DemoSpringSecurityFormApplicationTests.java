0d8d38e0b412358911b5222840741b9582a2cece
testMinseok <opso_@naver.com>
2022-09-03 00:52:34 +0900

    Created Project

---
+++ springSecurity/demo-spring-security-form/src/test/java/me/testmain/demospringsecurityform/DemoSpringSecurityFormApplicationTests.java
@@ -0,0 +1,13 @@
+package me.testmain.demospringsecurityform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringSecurityFormApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
