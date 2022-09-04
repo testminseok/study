d944c9b7c44108b99e6fa02b811fe39ab00ff7d1
testMinseok <opso_@naver.com>
2022-09-05 01:52:08 +0900

    Sprig Security Form Login 테스트 코드 작성

--- springSecurity/demo-spring-security-form/src/test/java/me/testmain/demospringsecurityform/account/AccountControllerTest.java
+++ springSecurity/demo-spring-security-form/src/test/java/me/testmain/demospringsecurityform/account/AccountControllerTest.java
@@ -9,9 +9,13 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.transaction.annotation.Transactional;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.anonymous;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -21,6 +25,9 @@ class AccountControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    @Autowired
+    AccountService accountService;
 
     /*
      * 인증되어 있지않은 사용자가 "/" 에 요청했을때
@@ -104,5 +111,41 @@ class AccountControllerTest {
         mockMvc.perform(MockMvcRequestBuilders.get("/admin"))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(status().isOk());
+    }
+
+    /*
+    * 정상적인 정보로 인증을 요청할경우 성공
+    * */
+    @Test
+    @Transactional
+    public void login_success() throws Exception {
+        String username = "testmin";
+        String password = "123123";
+        createUser(username, password);
+
+        mockMvc.perform(formLogin().user(username).password(password))
+                .andExpect(authenticated());
+    }
+
+    /*
+    * 잘못된 정보로 인증을 요청하는경우 인증 실패
+    * */
+    @Test
+    @Transactional
+    public void login_fail() throws Exception {
+        String username = "testmin";
+        String password = "123123";
+        createUser(username, password);
+
+        mockMvc.perform(formLogin().user(username).password("123344"))
+                .andExpect(unauthenticated());
+    }
+
+    private Account createUser(String username, String password) {
+        Account account = new Account();
+        account.setUsername(username);
+        account.setPassword(password);
+        account.setRole("USER");
+        return accountService.createNew(account);
     }
 }
