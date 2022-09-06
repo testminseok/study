b943c4f3e73043863e23d38057698ce4ef8aedad
testMinseok <opso_@naver.com>
2022-09-07 01:23:36 +0900

    SpringSecurity AuthenticationManager 를 통한 인증 진행 순서

--- springSecurity/demo-spring-security-form/src/main/java/me/testmain/demospringsecurityform/form/SampleService.java
+++ springSecurity/demo-spring-security-form/src/main/java/me/testmain/demospringsecurityform/form/SampleService.java
@@ -17,6 +17,13 @@ public class SampleService {
         * principal 객체는 User 타입이다.
         * authorities 안에는 사용자 권한이 담겨있고, ROLE_권한이름 의 형태로 담겨있다.
         * isAuthenticated 는 토큰 만료 같은 경우가 아니면 logout 하기 전까지 true 이다.
+        *
+        * Authentication 객체가 SecurityContextHolder.getContext().getAuthentication() 에 담기기 까지의 과정
+        * 실제로 인증은 AuthenticationManager 라는 인터페이스가 담당하게 되는데
+        * 이것을 구현한 ProviderManager.authenticate(Authentication authentication) 메소드에서 인증과정을 진행한다.
+        * 그 중 form 로그인을 인증할 수 있는 DaoAuthenticationProvider 클래스의 retrieveUser() 메소드로 사용자를 조회하게 되는데
+        * 이때 개발자가 UserDetailService 를 구현하여 Bean 으로 등록된 객체를 사용하여 사용자를 조회한다.
+        * 조회한 후 사용자의 계정이 잠겼는지, 비활성화 되었는지, 만료되었는지 검증한 후 사용자정보가 들어있는 Authentication 객체를 반환한다.
         * */
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         Object principal = authentication.getPrincipal();
