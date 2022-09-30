b12268e086e909847387bed24b307cb0d7ee85a5
testMinseok <opso_@naver.com>
2022-09-30 23:07:46 +0900

    SpringSecurity JwtServer 회원가입

--- springSecurity/jwt-server/src/main/java/com/testmin/jwtserver/controller/RestApiController.java
+++ springSecurity/jwt-server/src/main/java/com/testmin/jwtserver/controller/RestApiController.java
@@ -1,11 +1,24 @@
 package com.testmin.jwtserver.controller;
 
+import com.testmin.jwtserver.model.Account;
+import com.testmin.jwtserver.repository.AccountRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestApiController {
+
+    private final AccountRepository accountRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    public RestApiController(AccountRepository accountRepository, PasswordEncoder passwordEncoder) {
+        this.accountRepository = accountRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @GetMapping("/home")
     public String home() {
@@ -15,5 +28,12 @@ public class RestApiController {
     @PostMapping("/token")
     public String token() {
         return "<h1>token</h1>";
+    }
+
+    @PostMapping("/signup")
+    public String signup(@RequestBody Account account) {
+        Account user = new Account(account.getId(), account.getUsername(), passwordEncoder.encode(account.getPassword()), "ROLE_USER");
+        accountRepository.save(user);
+        return "ok";
     }
 }
