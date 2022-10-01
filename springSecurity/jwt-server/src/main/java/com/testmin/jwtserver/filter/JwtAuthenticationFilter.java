cb153c3a6865fb0ef18d9facdd33879efd3a1c14
testMinseok <opso_@naver.com>
2022-10-01 19:20:23 +0900

    SpringSecurity JwtServer Login Response Jwt
    로그인 완료 시 JWT 값을 리턴하게 변경

--- springSecurity/jwt-server/src/main/java/com/testmin/jwtserver/filter/JwtAuthenticationFilter.java
+++ springSecurity/jwt-server/src/main/java/com/testmin/jwtserver/filter/JwtAuthenticationFilter.java
@@ -1,18 +1,24 @@
 package com.testmin.jwtserver.filter;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.testmin.jwtserver.auth.AccountDetails;
 import com.testmin.jwtserver.model.Account;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
 
 /*
  * spring security 에서는 form 로그인을 담당하는 필터가 UsernamePasswordAuthenticationFilter 이다.
@@ -47,5 +53,18 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        AccountDetails accountDetails = (AccountDetails) authResult.getPrincipal();
+
+        String jwtToken = JWT.create()
+                .withSubject("testminToken")
+                .withExpiresAt(new Date(System.currentTimeMillis() + (60000 * 30)))
+                .withClaim("username", accountDetails.getUsername())
+                .sign(Algorithm.HMAC512("mySecretKey"));
+
+        response.addHeader(HttpHeaders.AUTHORIZATION, "Bearer " + jwtToken);
     }
 }
