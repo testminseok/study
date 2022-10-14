28c4dd2e205572af7e84d7e605ac4b8b0afaf4ae
testMinseok <opso_@naver.com>
2022-10-14 23:22:35 +0900

    ModernJavaInAction Stream 필터링 슬라이싱

---
+++ modernJavaInAction/src/examples/chap05/Filtering.java
@@ -0,0 +1,52 @@
+package examples.chap05;
+
+import examples.chap04.Dish;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Filtering {
+    public static void main(String[] args) {
+        // Predicate 로 filtering
+        List<Dish> vegetarianMenu =
+                Dish.menu.stream()
+                        .filter(Dish::isVegetarian)
+                        .collect(Collectors.toList());
+
+        // 고유 요소 필터링
+        List<Integer> numbers = List.of(1, 2, 1, 3, 3, 2, 4);
+        /*
+        * distinct 메서드의 고유 여부는 스트림에서 만든 객체의 hashCode 와 equals 메서드로 판단한다.
+        * */
+        numbers.stream()
+                .filter(i -> i % 2 == 0)
+                .distinct()
+                .forEach(System.out::println);
+
+        // 스트림 슬라이싱
+        List<Dish> specialMenu = List.of(
+                new Dish("season fruit", true, 120, Dish.Type.OTHER),
+                new Dish("prawns", false, 300, Dish.Type.FISH),
+                new Dish("rice", true, 350, Dish.Type.OTHER),
+                new Dish("chicken", false, 400, Dish.Type.MEAT),
+                new Dish("french fries", true, 530, Dish.Type.OTHER)
+        );
+
+        // 320 칼로리 이하의 음식 선택
+        specialMenu.stream()
+                .filter(d -> d.getCalories() < 320)
+                .collect(Collectors.toList());
+
+        // 이미 정렬되어있는 경우에 320칼로리보다 크거나 같은요리가 나왔을 때 takeWhile 을 사용하여 반복작업을 중단할 수 있다.
+        specialMenu.stream()
+                .takeWhile(d -> d.getCalories() < 320)
+                .collect(Collectors.toList());
+
+        // dropWhile 은 takeWhile 의 반대로 320칼로리보다 크거나 같은 음식이 나올 때까지 건너뛴다.
+        specialMenu.stream()
+                .dropWhile(d -> d.getCalories() < 320)
+                .collect(Collectors.toList());
+
+
+    }
+}
