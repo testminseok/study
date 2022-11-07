a8fe3c11937b47852225c284c27ff1209fba115d
testmin <opso_@naver.com>
2022-11-07 16:14:21 +0900

    ModernJavaInAction Map fatory method

--- modernJavaInAction/src/main/java/examples/chap08/CreatingCollections.java
+++ modernJavaInAction/src/main/java/examples/chap08/CreatingCollections.java
@@ -11,21 +11,30 @@ public class CreatingCollections {
         friends.add("Olivia");
         friends.add("Thibaut");
 
-        // Arrays.asList 팩토리 메소드 - 요소의 갱신은 되지만 크기가 고정되어 있으므로, 추가는 되지 않는다.
+        // Arrays.asList 팩토리 메소드 - (수정 가능, 추가 불가능)
         List<String> friends2 = Arrays.asList("Raphael", "Olivia", "Thibaut");
 
-        // set 이라는 팩토리 메소드는 없으므로 HashSet 의 생성자를 사용
+        // (수정, 추가 가능)
         Set<String> friends3 = new HashSet<>(Arrays.asList("Raphael", "Olivia", "Thibaut"));
 
-
+        // Set.of - (추가 가능, 중복 값 불가능)
         Set<String> friends4 = Stream.of("Raphael", "Olivia", "Thibaut")
                 .collect(Collectors.toSet());
 
-        // List.of 또한 추가가 불가능하다.
+        // List.of - (수정 불가능, 추가 불가능)
         List<String> friends5 = List.of("Raphael", "Olivia", "Thibaut");
 
-        // Set.of 추가가 불가능하며, 중복된 값이 있을경우도 IllegalArgumentException 를 발생시킨다.
+        // Set.of - (추가 불가능, 중복 값 불가능)
         Set<String> friends6 = Set.of("Raphael", "Olivia", "Thibaut");
 
+        // Map.of (key1,value1,key2,value2)
+        Map<String, Integer> ageOfFriends = Map.of("Raphael", 30, "Olivia", 25, "Thibaut", 26);
+
+        // Map.ofEntries
+        Map<String, Integer> ageOfFriends2 =
+                Map.ofEntries(
+                        Map.entry("Raphael", 30),
+                        Map.entry("Olivia", 25),
+                        Map.entry("Thibaut", 26));
     }
 }
