cd172a8100b6c2d0026605f6b3b3b2dcd415e112
testMinseok <opso_@naver.com>
2022-11-27 23:07:15 +0900

    ModernJavaInAction Optional 합치기 퀴즈

--- modernJavaInAction/src/main/java/examples/chap11/OptionalMain.java
+++ modernJavaInAction/src/main/java/examples/chap11/OptionalMain.java
@@ -78,4 +78,29 @@ public class OptionalMain {
                 .flatMap(Optional::stream) // (isPresent -> map) 의 과정을 거친것과 동일한 결과를 반환한다.
                 .collect(Collectors.toSet());
     }
+
+    /*
+    * Optional 을 사용할 때 주의할 점은
+    * Optional 자체가 null 일 수 있기에 null 체크를 하는 로직이 추가되어야 하므로 매게변수로 Optional 을 받으면 안된다.
+    * */
+    public Optional<Insurance> nullSafeFindCheapestInsurance(Optional<PersonV2> person, Optional<CarV2> car) {
+        if (person.isPresent() && car.isPresent()) {
+            return Optional.of(findCheapestInsurance(person.get(), car.get()));
+        }
+
+        return Optional.empty();
+    }
+
+    private Insurance findCheapestInsurance(PersonV2 person, CarV2 car) {
+        // 비즈니스 로직
+
+        return new Insurance();
+    }
+
+    /*
+     * 퀴즈 - Optional 언랩하지 않고 두 Optional 합치기
+     * */
+    public Optional<Insurance> quiz(Optional<PersonV2> person, Optional<CarV2> car) {
+        return person.flatMap(p -> car.map(c -> findCheapestInsurance(p, c)));
+    }
 }
