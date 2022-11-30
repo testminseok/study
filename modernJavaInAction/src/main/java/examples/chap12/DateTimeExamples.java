2beca117efa1f6766d9659ee2a24d99b086a801c
testmin <opso_@naver.com>
2022-11-30 17:40:45 +0900

    ModernJavaInAction LocalDate

---
+++ modernJavaInAction/src/main/java/examples/chap12/DateTimeExamples.java
@@ -0,0 +1,33 @@
+package examples.chap12;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.temporal.ChronoField;
+
+public class DateTimeExamples {
+    public static void main(String[] args) {
+
+        // 자바 8
+        LocalDate date = LocalDate.of(2022, 11, 30);
+        int year = date.getYear();
+        int yearByTemporalField = date.get(ChronoField.YEAR);
+        Month month = date.getMonth();
+        int monthByTemporalField = date.get(ChronoField.MONTH_OF_YEAR);
+        int day = date.getDayOfMonth();
+        int dayByTemporalField = date.get(ChronoField.DAY_OF_MONTH);
+        DayOfWeek dow = date.getDayOfWeek();
+        int len = date.lengthOfMonth();
+        boolean leap = date.isLeapYear();
+
+        System.out.println(year); // 2022
+        System.out.println(yearByTemporalField); // 2022
+        System.out.println(month); // NOVEMBER
+        System.out.println(monthByTemporalField); // 11
+        System.out.println(day); // 30
+        System.out.println(dayByTemporalField); // 30
+        System.out.println(dow); // WEDNESDAY
+        System.out.println(len); // 30 (11월의 일수)
+        System.out.println(leap); // false - 윤년이 아님
+    }
+}
