affbfad6bc1e944536e6e6b4cbeef663df104cab
testmin <opso_@naver.com>
2022-12-07 17:57:29 +0900

    ModernJavaInAction 디폴트 메소드 사용법과 추상클래스 와 인터페이스 비교

--- modernJavaInAction/src/main/java/examples/chap13/Resizable.java
+++ modernJavaInAction/src/main/java/examples/chap13/Resizable.java
@@ -1,6 +1,7 @@
 package examples.chap13;
 
 public interface Resizable extends Drawable {
+
     int getWidth();
 
     int getHeight();
@@ -10,5 +11,18 @@ public interface Resizable extends Drawable {
     void setHeight(int height);
 
     void setAbsoluteSize(int width, int height);
+
+    /*
+    * 자바 8에 추가된 interface default method
+    *
+    * 추상 클래스와 인터페이스의 차이점
+    * 1. 클래스는 하나의 클래스만 상송받을 수 있지만 인터페이스는 다중 상속이 가능하다.
+    * 2. 추상클래스는 인스턴스변수를 가질 수 있지만 인터페이스는 가질 수 없다.
+    * 3. 추상클래스는 생성자를 가질 수 있지만 인터페이스는 가질 수 없다.
+    * 4. 존재 목적이 다르다 추상 클래스는 확장이 목적이지만, 인터페이스는 명세에 목적이 있다.
+    * */
+    default void setRelativeSize(int wFactor, int hFactor) {
+        setAbsoluteSize(getWidth() / wFactor, getHeight() / hFactor);
+    }
 
 }
