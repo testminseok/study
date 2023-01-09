0e02fd0ad1b3a39e82b1ccd7d6f72a722b46cafa
testmin <opso_@naver.com>
2023-01-09 11:27:44 +0900

    ModernJavaInAction 방문자 디자인 패턴 코멘트 추가

--- modernJavaInAction/src/main/java/examples/chap19/patternmatching/SimplifyExprVisitor.java
+++ modernJavaInAction/src/main/java/examples/chap19/patternmatching/SimplifyExprVisitor.java
@@ -1,5 +1,11 @@
 package examples.chap19.patternmatching;
 
+/**
+ * 방문자 디자인 패턴은
+ * 특정 데이터 형식을 '방문'하는 알고리즘을 캡슐화 하는 클래스를 따로 만들 수 있다.
+ * 이렇게 분리를 하면 구조를 수정하지 않고도 실질적으로 새로운 동작을 기존의 객체 구조에 추가할 수 있게 된다.
+ * 개방-폐쇄 원칙을 적용하는 방법의 하나이다.
+ * */
 public class SimplifyExprVisitor {
 
     public Expr visit(BinOp binOp) {
