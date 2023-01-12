31b17c2eddc390d972c8ebe443a44e9ccf4c8cf1
testmin <opso_@naver.com>
2023-01-12 17:00:58 +0900

    ModernJavaInAction 스킬라 코드

---
+++ modernJavaInAction/src/main/java/examples/Foo.java
@@ -0,0 +1,19 @@
+package examples;
+
+import java.util.stream.IntStream;
+
+public class Foo {
+    /*
+    * 아래 코드와 동일한 결과의 스칼라 코드
+    * object Beer {
+    *   def main(args: Array[String]) {
+    *       2 to 6 foreach { n => println(s"Hello ${n} bottles of beer") }
+    *   }
+    * }
+    * */
+    public static void main(String[] args) {
+        IntStream.rangeClosed(2, 6)
+                .forEach(n -> System.out.println("Hello " + n + " bottles of beer"));
+
+    }
+}
