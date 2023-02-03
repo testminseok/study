7168a5f0d43b9cd02176262fc37ccdcc7864419e
testMinseok <opso_@naver.com>
2023-02-04 00:01:10 +0900

    javaCodingLikeThis try-catch-resource

--- javaCodingLikeThis/src/main/java/com/software/basic/problem/FinallySample.java
+++ javaCodingLikeThis/src/main/java/com/software/basic/problem/FinallySample.java
@@ -1,5 +1,10 @@
 package com.software.basic.problem;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * 1-17 finally 절에서는 return 을 사용하지 않는다.
  * - ReturnFromFinallyBlock : finally 에서 return 을 사용하는 문제점을 진단하고 수정을 권고한다.
@@ -35,5 +40,24 @@ public class FinallySample {
         } catch (Exception e) {
             throw e;
         }
+    }
+
+    /**
+     * 일반적으로 finally 절에서는 리소스의 close 메소드를 많이 호출하지만
+     * 자바 7 부터는 try-catch-resource 를 제공하여 Closeable 인터페이스를 상속한 클래스에 경우
+     * 자동으로 close 메소드를 호출해준다.
+     * */
+    public String getFileAsList(String path) {
+        StringBuilder result = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(path))) {
+            String line = null;
+            while ((line = in.readLine()) != null) {
+                result.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result.toString()
     }
 }
