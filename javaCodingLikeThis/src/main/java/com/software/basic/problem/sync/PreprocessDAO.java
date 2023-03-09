2b7aa694c2adcf22df936b78077f13ba91b54cec
testMinseok <opso_@naver.com>
2023-03-09 21:27:55 +0900

    javaCodingLikeThis 너무 많은 기능을 한 클래스에 넣지 않는다. 파일 누락 추가

---
+++ javaCodingLikeThis/src/main/java/com/software/basic/problem/sync/PreprocessDAO.java
@@ -0,0 +1,15 @@
+package com.software.basic.problem.sync;
+
+public class PreprocessDAO extends BaseDAO{
+    public PreprocessDAO() {
+    }
+
+    public void deleteOriginalData() {
+    }
+
+    public void getObjectidDupe() {
+    }
+
+    public void getObjectidDupeEquals() {
+    }
+}
