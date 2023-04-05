d4654c21c0f59d9495d9b6df6c937218e07df358
testMinseok <opso_@naver.com>
2023-04-05 20:06:58 +0900

    javaCodingLikeThis Thread의 run 메소드 대신 start 메소드를 사용한다

---
+++ javaCodingLikeThis/src/main/java/com/software/basic/problem/thread/ThreadExample.java
@@ -0,0 +1,17 @@
+package com.software.basic.problem.thread;
+
+/**
+ * 11-10 Thread 의 run 메소드 대신 start 메소드를 사용한다.
+ * DontCallThreadRun : Thread 의 run 메소드를 사용하는 경우 수정을 권고한다.
+ *                     스레드의 run 메소드를 호출하면, 스레드가 아닌 호출자의 제어 스레드에서 실행된다.
+ *                     즉, 일반 메소드로 동작한다.
+ * */
+public class ThreadExample {
+    public static void main(String[] args) {
+        Runnable runnable = new ThreadRunSample();
+        Thread thread = new Thread(runnable);
+        System.out.println("스레드 호출 시작");
+        thread.run();
+        System.out.println("스레드 호출 종료");
+    }
+}
---
+++ javaCodingLikeThis/src/main/java/com/software/basic/problem/thread/ThreadRunSample.java
@@ -0,0 +1,14 @@
+package com.software.basic.problem.thread;
+
+public class ThreadRunSample implements Runnable {
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(1000);
+            System.out.println(Thread.currentThread().getName() + " 작업이 실행됨");
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+}
