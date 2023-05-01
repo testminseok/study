32bce9e7c51b636ca341d8a79e64ef5a8f1ca809
testMinseok <opso_@naver.com>
2023-05-01 19:39:20 +0900

    effectiveJava item11 equals를 재정의하려거든 hashCode도 재정의하라

---
+++ effectiveJava/src/chapter02/item11/HashCodeExample.java
@@ -0,0 +1,21 @@
+package chapter02.item11;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashCodeExample {
+    public static void main(String[] args) {
+        final short areaCode = 707;
+        final short prefix = 867;
+        final short lineNum = 5309;
+
+        Map<PhoneNumber, String> m = new HashMap<>() {{
+            put(new PhoneNumber(areaCode, prefix, lineNum), "제니");
+        }};
+
+        // PhoneNumber 의 hashCode 를 재정의하지 않았다면 null 을 출력한다.
+        System.out.println(m.get(new PhoneNumber(areaCode, prefix, lineNum)));
+
+
+    }
+}
---
+++ effectiveJava/src/chapter02/item11/PhoneNumber.java
@@ -0,0 +1,62 @@
+package chapter02.item11;
+
+import java.util.Objects;
+
+public class PhoneNumber {
+    private final short areaCode;
+    private final short prefix;
+    private final short lineNum;
+
+    public PhoneNumber(short areaCode, short prefix, short lineNum) {
+        this.areaCode = rangeCheck(areaCode, 999, "지역코드");
+        this.prefix = rangeCheck(prefix, 999, "프리픽스");
+        this.lineNum = rangeCheck(lineNum, 9999, "가입자 번호");
+    }
+
+    private static short rangeCheck(int val, int max, String arg) {
+        if (val < 0 || val > max) {
+            throw new IllegalArgumentException(arg + ": " + val);
+        }
+
+        return (short) val;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        /*
+        * 1. == 연산자를 사용해 입력이 자기 자신의 참조인지 확인한다.
+        * 2. instanceof 연산자를 사용해 입력이 올바른 타입인지 확인한다.
+        * 3. 입력을 올바른 타입으로 형변환한다.
+        * 4. 입력 객체와 자기 자신의 대응되는 '핵심' 필드들이 모두 일치하는지 하나씩 검사한다.
+        *
+        * - float 와 double 필드는 각각 정정 메서드인 Float.compare(float, float) 와
+        *   Double.compare(double, double) 을 사용해 비교한다.
+        * - 배열의 모든 원소가 핵심 필드라면 Arrays.equals 를 사용한다.
+        * - 때론 null 도 정상 값으로 취금하는 참조 타입 필드도 있는데,
+        *   이런 필드는 정적 메서드인 Object.equals 를 사용해 NPE 를 방지한다.
+        *
+        * equals 를 다 구현했다면 세가지만 자문해보자. 대칭적인가?,  추이성이 있는가?, 일관적인가?
+        * */
+
+        // 1
+        if (this == o) return true;
+
+        // 2,3 instanceof 연산자는 이후 형변환이 안전한지를 보장한다.
+        if(!(o instanceof PhoneNumber)) {
+            return false;
+        }
+
+        // 4
+        PhoneNumber that = (PhoneNumber) o;
+        return areaCode == that.areaCode && prefix == that.prefix && lineNum == that.lineNum;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Short.hashCode(areaCode);
+        result = 31 * result + Short.hashCode(prefix);
+        result = 31 * result + Short.hashCode(lineNum);
+        return result;
+//        return Objects.hash(areaCode, prefix, lineNum); // 성능이 떨어진다. 때문에 성능에 민감하지 않은 상황에서만 사용하자.
+    }
+}
