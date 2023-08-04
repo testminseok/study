c8e8be6363bc4783d511d69d2dc05f24b4ac1388
testMinseok <opso_@naver.com>
2023-08-04 22:36:08 +0900

    baekjoon 11718

---
+++ algorithm/src/baekjoon/QuestionNo11718.java
@@ -0,0 +1,12 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class QuestionNo11718 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader inputStreamReader = new BufferedReader(new InputStreamReader(System.in));
+        inputStreamReader.lines().forEach(System.out::println);
+    }
+}
