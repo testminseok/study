3101b0373ec19ea7371e6701ee85180b35d5d7fa
testMinseok <opso_@naver.com>
2023-08-16 21:36:56 +0900

    baekjoon 2563

---
+++ algorithm/src/baekjoon/QuestionNo2563.java
@@ -0,0 +1,29 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class QuestionNo2563 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader inputStreamReader = new BufferedReader(new InputStreamReader(System.in));
+        boolean[][] map = new boolean[100][100];
+
+        int num = Integer.parseInt(inputStreamReader.readLine());
+        int count = 0;
+        for (int i = 0; i < num; i++) {
+            StringTokenizer stringTokenizer = new StringTokenizer(inputStreamReader.readLine(), " ");
+            int x = Integer.parseInt(stringTokenizer.nextToken());
+            int y = Integer.parseInt(stringTokenizer.nextToken());
+
+            for (int j = 0; j < 10; j++) {
+                for (int k = 0; k < 10; k++) {
+                    map[x + j][y + k] = true;
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
+}
