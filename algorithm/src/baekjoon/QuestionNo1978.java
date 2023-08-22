9567d16ba870f3d5493613cd4512fbc51597dc4a
testMinseok <opso_@naver.com>
2023-08-22 18:17:06 +0900

    baekjoon 1978

---
+++ algorithm/src/baekjoon/QuestionNo1978.java
@@ -0,0 +1,33 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class QuestionNo1978 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader inputStreamReader = new BufferedReader(new InputStreamReader(System.in));
+        int size = Integer.parseInt(inputStreamReader.readLine());
+        StringTokenizer stringTokenizer = new StringTokenizer(inputStreamReader.readLine(), " ");
+        int sum = 0;
+        for (int i = 0; i < size; i++) {
+            int num = Integer.parseInt(stringTokenizer.nextToken());
+
+            // 약수의 갯수
+            int total = 0;
+            for (int j = 1; j <= num; j++) {
+                if (num % j == 0) {
+                    total++;
+                }
+            }
+
+            // 소수는 1 과 자기 자신 만을 소수로 가진 수 (2개)
+            if (total == 2) {
+                sum++;
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
