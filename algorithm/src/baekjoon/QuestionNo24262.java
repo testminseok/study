753472dbf07b39c666669f9f845046742399a42d
testMinseok <opso_@naver.com>
2023-09-03 23:39:31 +0900

    baekjoon 24262

---
+++ algorithm/src/baekjoon/QuestionNo24262.java
@@ -0,0 +1,8 @@
+package baekjoon;
+
+public class QuestionNo24262 {
+    public static void main(String[] args) throws Exception {
+        System.out.println(1);
+        System.out.println(0); // O(1) = 0, O(n) = 1, O(n^2) = 2, O(n^3) = 3, 그 이상은 4
+    }
+}
