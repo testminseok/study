73fe1f155a6f495430cbdf8e79e8dee9e7ea87e1
testMinseok <opso_@naver.com>
2023-09-04 20:09:38 +0900

    baekjoon 24263

---
+++ algorithm/src/baekjoon/QuestionNo24263.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class QuestionNo24263 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int size = Integer.parseInt(bufferedReader.readLine());
+        System.out.println(size);
+        System.out.println(1);
+    }
+}
