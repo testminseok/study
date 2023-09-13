348993ff043cd3386ab4c25f84bd2b15bbac6414
testMinseok <opso_@naver.com>
2023-09-13 20:51:26 +0900

    baekjoon 2750

---
+++ algorithm/src/baekjoon/QuestionNo2750.java
@@ -0,0 +1,31 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class QuestionNo2750 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader inputStreamReader = new BufferedReader(new InputStreamReader(System.in));
+        int size = Integer.parseInt(inputStreamReader.readLine());
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            int num = Integer.parseInt(inputStreamReader.readLine());
+            arr[i] = num;
+        }
+
+        for (int i = 0; i < size; i++) {
+            for (int j = i + 1; j < size; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        for (int num : arr) {
+            System.out.println(num);
+        }
+    }
+}
