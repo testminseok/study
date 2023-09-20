92d5e64ea01ec70838a70bfaf499d2b652414cd9
testMinseok <opso_@naver.com>
2023-09-20 21:16:00 +0900

    baekjoon 11651

---
+++ algorithm/src/baekjoon/QuestionNo11651.java
@@ -0,0 +1,39 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class QuestionNo11651 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader inputStreamReader = new BufferedReader(new InputStreamReader(System.in));
+        int size = Integer.parseInt(inputStreamReader.readLine());
+        List<Map<String, Integer>> list = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
+            StringTokenizer stringTokenizer = new StringTokenizer(inputStreamReader.readLine(), " ");
+            int x = Integer.parseInt(stringTokenizer.nextToken());
+            int y = Integer.parseInt(stringTokenizer.nextToken());
+
+            HashMap<String, Integer> element = new HashMap<>();
+            element.put("x", x);
+            element.put("y", y);
+            list.add(element);
+        }
+
+        list.sort((x1, x2) -> {
+            int compare = Integer.compare(x1.get("y"), x2.get("y"));
+            if (compare == 0) {
+                return Integer.compare(x1.get("x"), x2.get("x"));
+            }
+            return compare;
+        });
+
+        StringBuilder builder = new StringBuilder();
+        for (Map<String, Integer> element : list) {
+            builder.append(element.get("x")).append(" ").append(element.get("y")).append("\n");
+        }
+        System.out.println(builder);
+    }
+}
