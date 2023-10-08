cc6a4ccee4de11a47a6a10578e66f9e6de8d76f6
testMinseok <opso_@naver.com>
2023-10-08 21:14:41 +0900

    baekjoon 4949

--- algorithm/src/baekjoon/QuestionNo4949.java
+++ algorithm/src/baekjoon/QuestionNo4949.java
@@ -3,28 +3,50 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class QuestionNo4949 {
     public static void main(String[] args) throws IOException {
         BufferedReader inputStreamReader = new BufferedReader(new InputStreamReader(System.in));
         String str;
         StringBuilder builder = new StringBuilder();
-        while (!(str = inputStreamReader.readLine()).equals(".")) {
-            str = str.replaceAll("[^()\\[\\]]", "");
-            if (str.length() % 2 != 0) {
-                builder.append("no").append("\n");
-                continue;
-            }
-            while (str.contains("()") || str.contains("[]")) {
-                str = str.replaceAll("\\(\\)", "").replaceAll("\\[]", "");
-            }
-            if (str.isEmpty()) {
-                builder.append("yes").append("\n");
-            } else {
-                builder.append("no").append("\n");
-            }
+        while(!(str = inputStreamReader.readLine()).equals(".")) {
+            builder.append(result(str)).append("\n");
         }
 
         System.out.println(builder);
+    }
+
+    private static String result(String str) {
+        Stack<Character> characters = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            char charAt = str.charAt(i);
+            if (charAt == '(' || charAt == '[') {
+                characters.push(charAt);
+                continue;
+            }
+
+            if (charAt == ')') {
+                if (characters.isEmpty() || characters.peek() != '(') {
+                    return "no";
+                } else {
+                    characters.pop();
+                }
+            }
+
+            if (charAt == ']') {
+                if (characters.isEmpty() || characters.peek() != '[') {
+                    return "no";
+                } else {
+                    characters.pop();
+                }
+            }
+        }
+
+        if (characters.isEmpty()) {
+            return "yes";
+        }
+
+        return "no";
     }
 }
