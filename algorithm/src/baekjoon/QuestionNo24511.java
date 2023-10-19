8d0ee4280405c69a4ada23136863fe7eec42af0b
testMinseok <opso_@naver.com>
2023-10-19 20:16:09 +0900

    baekjoon 24511

--- algorithm/src/baekjoon/QuestionNo24511.java
+++ algorithm/src/baekjoon/QuestionNo24511.java
@@ -11,28 +11,14 @@ public class QuestionNo24511 {
         int N = Integer.parseInt(inputStreamReader.readLine());
 
         StringTokenizer stringTokenizer = new StringTokenizer(inputStreamReader.readLine(), " ");
-        List<Collection<Integer>> collections = new ArrayList<>();
+        StringTokenizer stringTokenizer2 = new StringTokenizer(inputStreamReader.readLine(), " ");
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
         for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(stringTokenizer.nextToken());
-            if (num == 1) {
-                Stack<Integer> stack = new Stack<>();
-                collections.add(stack);
-            } else {
-                Queue<Integer> queue = new LinkedList<>();
-                collections.add(queue);
-            }
-        }
-
-        stringTokenizer = new StringTokenizer(inputStreamReader.readLine(), " ");
-        for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(stringTokenizer.nextToken());
-            Collection<Integer> collection = collections.get(i);
-            if (collection instanceof Stack) {
-                Stack<Integer> stack = (Stack<Integer>) collection;
-                stack.add(num);
-            } else {
-                Queue<Integer> queue = (Queue<Integer>) collection;
-                queue.add(num);
+            int type = Integer.parseInt(stringTokenizer.nextToken());
+            int num = Integer.parseInt(stringTokenizer2.nextToken());
+            // Stack 에 삽입된 요소는 사용되지 않으므로 Stack 자체를 제거
+            if (type == 0) {
+                deque.add(num);
             }
         }
 
@@ -41,19 +27,8 @@ public class QuestionNo24511 {
         StringBuilder builder = new StringBuilder();
         for (int i = 0; i < M; i++) {
             int num = Integer.parseInt(stringTokenizer.nextToken());
-            for (int j = 0; j < N; j++) {
-                Collection<Integer> collection = collections.get(j);
-                if (collection instanceof Stack) {
-                    Stack<Integer> stack = (Stack<Integer>) collection;
-                    stack.add(num);
-                    num = stack.pop();
-                } else {
-                    Queue<Integer> queue = (Queue<Integer>) collection;
-                    queue.add(num);
-                    num = queue.poll();
-                }
-            }
-            builder.append(num).append(" ");
+            deque.addFirst(num);
+            builder.append(deque.pollLast()).append(" ");
         }
 
         System.out.println(builder);
