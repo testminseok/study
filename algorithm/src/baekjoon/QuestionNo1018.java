32f108f189c50b9f1f0b52675f453f9ca70231a0
testMinseok <opso_@naver.com>
2023-10-31 21:47:24 +0900

    baekjoon 1018 리팩터링

--- algorithm/src/baekjoon/QuestionNo1018.java
+++ algorithm/src/baekjoon/QuestionNo1018.java
@@ -18,46 +18,42 @@ public class QuestionNo1018 {
             System.arraycopy(strings, 0, arr[i], 0, M);
         }
 
-
-
         int minValue = Integer.MAX_VALUE;
         for (int i = 0; i < N - 7; i++) {
             for (int j = 0; j < M - 7; j++) {
                 // 시작 색깔이 흰색인경우, 검은색인경우 둘다 구해 최소값 설정
-                minValue = Integer.min(cal("W", minValue, arr, i, j), cal("B", minValue, arr, i, j));
+                minValue = Integer.min(minValue,
+                        Integer.min(calculate("W", arr, i, j), calculate("B", arr, i, j)));
             }
         }
 
         System.out.println(minValue);
     }
 
-    private static int cal(String color, int minValue, String[][] arr, int i , int j) {
+    private static int calculate(String color, String[][] arr, int i , int j) {
+        int cnt = 0;
+        for (int k = 0; k < 8; k++) {
+            // 열의 수정 갯수를 구한다.
+            cnt += rowCalculate(color, arr[i + k], j);
+            // 시작색깔이 흰색이면 다음 줄은 검은색 부터 시작한다.
+            color = color.equals("W") ? "B" : "W";
+        }
+        return cnt;
+    }
+
+    private static int rowCalculate(String color, String[] arr, int j) {
         // 흰색 정상 패턴
         String[] white = new String[]{"W","B","W","B","W","B","W","B"};
         // 검은색 정상 패턴
         String[] black = new String[]{"B","W","B","W","B","W","B","W"};
+
         int cnt = 0;
-        for (int k = 0; k < 8; k++) {
-            if (color.equals("W")) {
-                for (int z = 0; z < 8; z++) {
-                    // 가로세로 비교하여 흰색 정상 패턴과 다른 갯수를 구한다.
-                    if (!arr[i + k][j + z].equals(white[z])) {
-                        cnt++;
-                    }
-                }
-                // 흰색의 다음 줄은 검은색이여야 함
-                color = "B";
-            } else {
-                for (int z = 0; z < 8; z++) {
-                    // 가로세로 비교하여 검은색 정상 패턴과 다른 갯수를 구한다.
-                    if (!arr[i + k][j + z].equals(black[z])) {
-                        cnt++;
-                    }
-                }
-                // 검은색의 다음 줄은 흰색이여야 함
-                color = "W";
+        String[] target = color.equals("W") ? white : black;
+        for (int i = 0; i < target.length; i++) {
+            if (!arr[i + j].equals(target[i])) {
+                cnt++;
             }
         }
-        return Integer.min(minValue, cnt);
+        return cnt;
     }
 }
