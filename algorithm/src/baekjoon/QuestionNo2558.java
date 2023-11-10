0c497b6a6226929a6d722256765c8a20fa4b55db
testMinseok <opso_@naver.com>
2023-11-10 21:44:31 +0900

    baekjoon 2558

--- algorithm/src/baekjoon/QuestionNo2558.java
+++ algorithm/src/baekjoon/QuestionNo2558.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class QuestionNo1085 {
+public class QuestionNo2558 {
     public static void main(String[] args) throws IOException {
         BufferedReader inputStreamReader = new BufferedReader(new InputStreamReader(System.in));
         int A = Integer.parseInt(inputStreamReader.readLine());
