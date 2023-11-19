8ccbc4617e5e94aae30003df48cfec5f5045b94d
testMinseok <opso_@naver.com>
2023-11-19 14:58:14 +0900

    baekjoon 5337

---
+++ algorithm/src/baekjoon/QuestionNo5337.java
@@ -0,0 +1,11 @@
+package baekjoon;
+
+public class QuestionNo5337 {
+    public static void main(String[] args) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(".  .   .").append("\n");
+        builder.append("|  | _ | _. _ ._ _  _").append("\n");
+        builder.append("|/\\|(/.|(_.(_)[ | )(/.").append("\n");
+        System.out.println(builder);
+    }
+}
