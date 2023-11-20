296d7c34e4e8b057c6031639f68804c17e703a5a
testMinseok <opso_@naver.com>
2023-11-20 22:05:11 +0900

    baekjoon 5338

---
+++ algorithm/src/baekjoon/QuestionNo5338.java
@@ -0,0 +1,12 @@
+package baekjoon;
+
+public class QuestionNo5338 {
+    public static void main(String[] args) {
+        String windowsLogo = "       _.-;;-._" + "\n" +
+                "'-..-'|   ||   |" + "\n" +
+                "'-..-'|_.-;;-._|" + "\n" +
+                "'-..-'|   ||   |" + "\n" +
+                "'-..-'|_.-''-._|" + "\n";
+        System.out.println(windowsLogo);
+    }
+}
