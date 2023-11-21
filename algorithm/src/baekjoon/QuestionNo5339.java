13aa1e17a7e15b7414b567a11174a315d3d3f601
minwooKim <rlaalsdn8@gmail.com>
2023-11-21 21:37:42 +0900

    baekjoon 5339

---
+++ algorithm/src/baekjoon/QuestionNo5339.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+public class QuestionNo5339 {
+    public static void main(String[] args) {
+        String robot = "     /~\\\n" +
+                "    ( oo|\n" +
+                "    _\\=/_\n" +
+                "   /  _  \\\n" +
+                "  //|/.\\|\\\\\n" +
+                " ||  \\ /  ||\n" +
+                "============\n" +
+                "|          |\n" +
+                "|          |\n" +
+                "|          |";
+        System.out.println(robot);
+    }
+}
