6965d86a3909be2ade0c32235ced577826372c7c
testMinseok <opso_@naver.com>
2023-11-27 18:13:39 +0900

    baekjoon 13277

---
+++ algorithm/src/baekjoon/QuestionNo13277.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class QuestionNo13277 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader inputStreamReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(inputStreamReader.readLine());
+        BigInteger A = new BigInteger(stringTokenizer.nextToken());
+        BigInteger B = new BigInteger(stringTokenizer.nextToken());
+
+        System.out.println(A.multiply(B));
+    }
+}
