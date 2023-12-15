a9253f8e93e6136d1d3ae19671bcb4c0e0e41e16
testMinseok <opso_@naver.com>
2023-12-15 20:40:54 +0900

    baekjoon 2552

---
+++ algorithm/src/baekjoon/QuestionNo2522.java
@@ -0,0 +1,33 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class QuestionNo2522 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader inputStreamReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(inputStreamReader.readLine());
+
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            // 공백입력
+            builder.append(" ".repeat(N - (i + 1)));
+            // 별표입력
+            builder.append("*".repeat(i + 1));
+            // 공백입력
+            builder.append("\n");
+        }
+
+        for (int i = 1; i < N; i++) {
+            // 공백입력
+            builder.append(" ".repeat(i));
+            // 별표입력
+            builder.append("*".repeat(N - i));
+            // 공백입력
+            builder.append("\n");
+        }
+
+        System.out.println(builder);
+    }
+}
