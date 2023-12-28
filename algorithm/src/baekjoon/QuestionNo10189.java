4e496630f4c138bc3f5f2e16fea117fbf9797645
testMinseok <opso_@naver.com>
2023-12-28 19:40:03 +0900

    baekjoon 10189

---
+++ algorithm/src/baekjoon/QuestionNo10189.java
@@ -0,0 +1,11 @@
+package baekjoon;
+
+public class QuestionNo10189 {
+    public static void main(String[] args) {
+        String hook = "#  # #### #### #  #\n" +
+                "#### #  # #  # # #\n" +
+                "#### #  # #  # # #\n" +
+                "#  # #### #### #  #";
+        System.out.println(hook);
+    }
+}
