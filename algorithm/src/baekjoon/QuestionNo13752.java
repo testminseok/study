bd921e645872fe5eeddea5adbfe6d82101728f4b
testMinseok <opso_@naver.com>
2024-01-20 23:42:06 +0900

    baekjoon 13752

---
+++ algorithm/src/baekjoon/QuestionNo13752.java
@@ -0,0 +1,20 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class QuestionNo13752 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader inputStreamReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(inputStreamReader.readLine());
+
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            int size = Integer.parseInt(inputStreamReader.readLine());
+            builder.append("=".repeat(size)).append("\n");
+        }
+
+        System.out.println(builder);
+    }
+}
