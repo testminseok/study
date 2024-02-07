78537af4e02bc87b19fece08b4d979a68ec9ab4c
testMinseok <opso_@naver.com>
2024-02-07 15:20:33 +0900

    baekjoon 14581

---
+++ algorithm/src/baekjoon/QuestionNo14581.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class QuestionNo14581 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader inputStreamReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.printf(":fan::fan::fan:\n" +
+                ":fan::%s::fan:\n" +
+                ":fan::fan::fan:", inputStreamReader.readLine());
+    }
+}
